1da650d771e27c4299252b033a4fe050bc4f25ae
Zero-user <77414062+Zero3720@users.noreply.github.com>
2024-08-26 13:47:25 -0300

    Commit 26/08/2024

---
+++ Maze/Assets/RANDOM DELETABLE SHIT/TimerBehaviour.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerBehaviour : MonoBehaviour
+{
+    public float timeValue = 1800;//tempo total em segundos
+    public TextMeshProUGUI TimeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeValue>0){
+            timeValue -= Time.deltaTime;
+        }else{
+            timeValue = 0;
+        }
+
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float TimeToDisplay){
+        if(TimeToDisplay < 0){
+            TimeToDisplay = 0;
+        }
+        float minutes = Mathf.FloorToInt(TimeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(TimeToDisplay % 60);
+
+        TimeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
